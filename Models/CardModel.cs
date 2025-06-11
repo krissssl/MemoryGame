e37423a0550ebcd3842dbed1a89debe31c919f49
krissssl <152088898+krissssl@users.noreply.github.com>
2025-06-11 22:18:20 +0300

    Add files via upload

---
+++ Models/CardModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace MemoryGame.Models
+{
+    public class CardModel : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+
+        private bool _isFlipped;
+        public bool IsFlipped
+        {
+            get => _isFlipped;
+            set
+            {
+                _isFlipped = value;
+                OnPropertyChanged(nameof(IsFlipped));
+            }
+        }
+
+        private bool _isMatched;
+        public bool IsMatched
+        {
+            get => _isMatched;
+            set
+            {
+                _isMatched = value;
+                OnPropertyChanged(nameof(IsMatched));
+            }
+        }
+
+        public CardModel(int id, string symbol)
+        {
+            Id = id;
+            Symbol = symbol;
+            IsFlipped = false;
+            IsMatched = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
+
+
